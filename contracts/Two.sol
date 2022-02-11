9f6ae6a06bf21dba4efc301457162b20651ac3e7
kyunhyun94 <ekfqlwcjswl@naver.com>
2022-02-11 17:23:01 +0900

    ch8

--- contracts/Two.sol
+++ contracts/Two.sol
@@ -2,3 +2,14 @@ pragma solidity ^0.8.11;
 //다른파일에서 불러올 때
 import "./One.sol";
 
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
+  }
+
+}
