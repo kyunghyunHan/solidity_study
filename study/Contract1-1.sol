08f99d3015ea12a1db0fac17ef433e059d77a4f3
kyunhyun94 <ekfqlwcjswl@naver.com>
2022-05-07 21:49:05 +0900

    Update Contract1-1.sol

--- study/Contract1-1.sol
+++ study/Contract1-1.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
+    // 여기에 이벤트 선언
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -12,8 +14,19 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string _name, uint _dna) {
+    function _createZombie(string _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+        // 여기서 이벤트 실행
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
